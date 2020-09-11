aa12e71205add2095a9c81444cd62456d9414099
Caio Cordeiro <ccmr@cesar.org.br>
2020-09-11 12:26:19 -0300

    Done

--- tests/compile_test_prod.sh
+++
@@ -1,3 +0,0 @@
-gcc -o integration_test_prod utils.c integration_test_prod.c -lrabbitmq -lknotcloudsdkc -lknotprotocol -lell
-valgrind --leak-check=full  --track-origins=yes --track-fds=yes ./integration_test_prod
-#./integration_test_prod
